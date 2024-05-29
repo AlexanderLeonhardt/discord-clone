780295a45ead8f39f74b1336e8525006bb1cbe44
Alex <alexpleonhardt@gmail.com>
2024-05-29 13:24:52 -0400

    Changed friends to messages
    The friends list exists elsewhere. This should be a list of messages.

---
+++ src/components/aside/messages/Messages.tsx
@@ -0,0 +1,11 @@
+import styles from "./messages.module.css";
+
+const Messages = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default Messages;
