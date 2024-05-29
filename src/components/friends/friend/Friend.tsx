f1b6689ee1bd02fa5466c7dcb21a566bee91a40c
Alex <alexpleonhardt@gmail.com>
2024-05-29 13:34:24 -0400

    Friends setup

---
+++ src/components/friends/friend/Friend.tsx
@@ -0,0 +1,11 @@
+import styles from "./friend.module.css"
+
+const Friend = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default Friend;
---
+++ src/components/friends/searchFriend/SearchFriend.tsx
@@ -0,0 +1,11 @@
+import styles from "./searchFriend.module.css"
+
+const SearchFriend = () => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    );
+}
+
+export default SearchFriend;
